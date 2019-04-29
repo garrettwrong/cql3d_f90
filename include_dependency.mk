957174960c154f35a816908b75ec869053622946
Garrett Wright <gwright@pppl.gov>
2019-04-29 12:57:50 -0400

    adding files for makes

---
+++ include_dependency.mk
@@ -0,0 +1,5 @@
+# I have used h77 to denote f77 formatted header
+# while .h is f90.
+# as the need for f77 goes away, then will call everything .h again...
+INCLUDES  = frcomm.h77 frname.h77 frname_decl.h77 name.h77 \
+	frname.h frname_decl.h name.h trans.h wpadvnc.h
