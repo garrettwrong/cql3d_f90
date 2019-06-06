298ab18dac34eec4fdb3c48c07385e13c7a1d1d7
Garrett Wright <gwright@pppl.gov>
2019-06-06 20:54:38 +0000

    yuri says these are not used.

--- wpadvnc.h
+++ wpadvnc.h
@@ -14,17 +14,6 @@
       fsmid(i,j,k,l)=fnp1(i,j,k,l)*dls(i,j,k,l) + &
       fnp1(ilpm1ef(i,l,+1),j,k,lpm1eff(l,+1))*(1-dls(i,j,k,l))
 
-!     A(f)=-u_par*df/ds (at cst i)
-!      wpa(i,j,k,l)=wpweigt(i,j,l)*(fsmid(i,j,k,l) - &
-!       fsmid(ilpm1ef(i,l,-1),j,k,lpm1eff(l,-1)))
-!      wpap(i,j,k,l)=cvmgt(wpweigp(i,j,l) * &
-!       (fnp1(ilpm1ef(i,l,+1),j,k,lpm1eff(l,+1))-fnp1(i,j,k,l)), &
-!       zero,ilpm1ef(i,l,+1).ne.-999)
-!      wpacen(i,j,k,l)=cvmgt(wpweigt(i,j,l) * &
-!       (fnp1(ilpm1ef(i,l,+1),j,k,lpm1eff(l,+1)) - &
-!       fnp1(ilpm1ef(i,l,-1),j,k,lpm1eff(l,-1))) , &
-!       zero,ilpm1ef(i,l,+1).ne.-999 .and. ilpm1ef(i,l,-1).ne.-999)
-
 !     contribution to matrix for f(s-1), f(s) and f(s+1)
       wprhsmd(i,j,k,l)=fnhalf(i,j,k,l)/dtreff+velsou(i,j,k,l)
       wprhs(i,j,k,l)=fnhalf(i,j,k,l)/dtreff + &
