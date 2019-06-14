4e82616b7fc5b88b692781c1b967f2d5415ed021
Garrett Wright <philomathas@gmail.com>
2019-06-14 18:42:50 -0400

    rm h77 from make includes

--- include_dependency.mk
+++ include_dependency.mk
@@ -1,6 +1,1 @@
-# I have used h77 to denote f77 formatted header
-# while .h is f90.
-# as the need for f77 goes away, then will call everything .h again...
-
-INCLUDES  = frcomm.h77 frname.h77 frname_decl.h77 \
-	frname.h frname_decl.h trans.h wpadvnc.h
+INCLUDES  = frname.h frname_decl.h name.h trans.h wpadvnc.h
