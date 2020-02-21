5cbd54cc5d0a9fa06f44a358a614f3147034e181
Garrett Wright <gwright@pppl.gov>
2020-02-21 10:48:38 -0500

    add doc string to cql nml conv rx

--- debug_cqlnml_converter.py
+++ debug_cqlnml_converter.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python
+'''
+
+In the process of running TRANSP, it can dump a debug nml.
+At the time of writing this dump is enabled for MPI runs, to assist transition.
+Developers may make this optional, perhaps only for debugging, in the future.
+
+This namelist comes _from within cql3d_f90_ and so uses f90 derived type syntax.
+
+The standalone xcql3d expects the legacy f77 syntax NML by request of Bob@CompX.
+
+You can use this tool to convet to the legacy style.
+
+Note, methods to read, write, and print both styles are implimented in the code.
+Future developers may change to the new style if they wish.
+
+You convert like so, assuming your dump is called debug_new:
+
+./debug_cqlnml_converter.py debug_new cqlinput
+
+I would also recomend that you set VERBOSE = 1, to re-enable debug prints.
+
+Good luck.
+
+'''
+
 from __future__ import print_function
 
 import sys
